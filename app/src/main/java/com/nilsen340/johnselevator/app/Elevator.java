dd0cef6a4358fd10a2d1446a633e4f07d14c4c2d
John Rikard Nilsen <nilsen340@gmail.com>
2014-04-26 10:08:58 +0200

    Linked Elevator to Engine class, implemented new features
    Elevator now
     - Uses engine events to know current floor
     - Fulfills one request at the time
     - Ignores requests while serving
    
    Elevator still lacks
     - An observable interface for listeners
     - Queueing requests
     - differentiate requests from inside/outside

--- app/src/main/java/com/nilsen340/johnselevator/app/Elevator.java
+++ app/src/main/java/com/nilsen340/johnselevator/app/Elevator.java
@@ -5,20 +5,32 @@ import java.util.Random;
 /**
  * Created by john on 23/04/14.
  */
-public class Elevator {
+public class Elevator implements Engine.EngineListener {
 
     public static final int NUM_FLOORS = 8;
 
     private int currentFloor;
+    private int wantedFloor;
     private MOVEMENT movement;
+    private Engine engine;
 
-    public Elevator(Random rand){
+    public Elevator(Random rand, Engine engine){
         currentFloor = rand.nextInt() % NUM_FLOORS;
         movement = MOVEMENT.STILL;
+        this.engine = engine;
+        this.engine.setListener(this);
     }
 
     public int getCurrentFloor(){
         return currentFloor;
+    }
+
+    public int getWantedFloor(){
+        return wantedFloor;
+    }
+
+    public void setWantedFloor(int wantedFloor){
+        this.wantedFloor = wantedFloor;
     }
 
     public MOVEMENT getMovement() {
@@ -26,13 +38,43 @@ public class Elevator {
     }
 
     public void requestElevatorToFloor(int floorNum) {
+        if(movement != MOVEMENT.STILL){
+            return;
+        }
+        wantedFloor = floorNum;
         if(floorNum < currentFloor) {
             movement = MOVEMENT.DOWN;
+            engine.goDownOneFloor();
             return;
         }
         if(floorNum > currentFloor) {
             movement = MOVEMENT.UP;
+            engine.goUpOneFloor();
         }
+    }
+
+    @Override
+    public void wentDownOneFloor() {
+        currentFloor--;
+        if(wantedFloor < currentFloor){
+            engine.goDownOneFloor();
+        } else {
+            wantedFloorReached();
+        }
+    }
+
+    @Override
+    public void wentUpOneFloor() {
+        currentFloor++;
+        if(wantedFloor > currentFloor){
+            engine.goUpOneFloor();
+        } else {
+            wantedFloorReached();
+        }
+    }
+
+    private void wantedFloorReached(){
+        movement = MOVEMENT.STILL;
     }
 
     public enum MOVEMENT {
--- app/src/test/java/com/nilsen340/johnselevator/app/ElevatorTest.java
+++ app/src/test/java/com/nilsen340/johnselevator/app/ElevatorTest.java
@@ -1,5 +1,7 @@
 package com.nilsen340.johnselevator.app;
 
+import com.nilsen340.johnselevator.app.testutil.SynchronousExecutorService;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,8 @@ import java.util.Random;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -17,13 +21,21 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricGradleTestRunner.class)
 public class ElevatorTest {
 
+    private static final int START_FLOOR = 4;
     Random rand = mock(Random.class);
+    Engine engine = mock(Engine.class);
+    Engine synchronizedEngine = new Engine(0);
+
+    //SUT
     Elevator elevator;
+    Elevator synchronizedElevator;
 
     @Before
     public void setUp(){
-        when(rand.nextInt()).thenReturn(Elevator.NUM_FLOORS + 4);
-        elevator = new Elevator(rand);
+        synchronizedEngine.setExecutor(new SynchronousExecutorService());
+        when(rand.nextInt()).thenReturn(Elevator.NUM_FLOORS + START_FLOOR);
+        elevator = new Elevator(rand, engine);
+        synchronizedElevator = new Elevator(rand, synchronizedEngine);
     }
 
     @Test
@@ -37,20 +49,71 @@ public class ElevatorTest {
     }
 
     @Test
-    public void whenElevatorReceivesFloorRequestBelowCurrentFloorElevatorMovesDown(){
-        elevator.requestElevatorToFloor(2);
+    public void whenElevatorReceivesFloorRequestBelowCurrentFloorElevatorSignalDown(){
+        elevator.requestElevatorToFloor(START_FLOOR - 2);
         assertThat(elevator.getMovement(), is(Elevator.MOVEMENT.DOWN));
     }
 
     @Test
-    public void whenElevatorReceivesFloorRequestAboveCurrentFloorElevatorMovesUp(){
-        elevator.requestElevatorToFloor(6);
+    public void whenElevatorReceivesFloorRequestAboveCurrentFloorElevatorSignalUp(){
+        elevator.requestElevatorToFloor(START_FLOOR + 2);
         assertThat(elevator.getMovement(), is(Elevator.MOVEMENT.UP));
     }
 
     @Test
+    public void whenElevatorReceivesFloorRequestOneBelowCurrentFloorChanges(){
+        synchronizedElevator.requestElevatorToFloor(START_FLOOR - 1);
+        assertThat(synchronizedElevator.getCurrentFloor(), is(START_FLOOR -1));
+    }
+
+    @Test
+    public void whenElevatorReceivesFloorRequestTwoBelowFloorChanges(){
+        synchronizedElevator.requestElevatorToFloor(START_FLOOR - 2);
+        assertThat(synchronizedElevator.getCurrentFloor(), is(START_FLOOR - 2));
+    }
+
+    @Test
+    public void whenElevatorReceivesFloorRequestTwoAboveFloorChanges(){
+        synchronizedElevator.requestElevatorToFloor(START_FLOOR + 2);
+        assertThat(synchronizedElevator.getCurrentFloor(), is(START_FLOOR + 2));
+    }
+
+    @Test
+    public void movementStillWhenElevatorReachesWantedFloorGoingDown(){
+        synchronizedElevator.requestElevatorToFloor(START_FLOOR - 1);
+        assertThat(synchronizedElevator.getMovement(), is(Elevator.MOVEMENT.STILL));
+    }
+
+    @Test
+    public void movementStillWhenElevatorReachesWantedFloorGoingUp(){
+        synchronizedElevator.requestElevatorToFloor(START_FLOOR + 1);
+        assertThat(synchronizedElevator.getMovement(), is(Elevator.MOVEMENT.STILL));
+    }
+
+    @Test
     public void whenElevatorRequestedToCurrentFloorMovementStill(){
-        elevator.requestElevatorToFloor(4);
+        elevator.requestElevatorToFloor(START_FLOOR);
         assertThat(elevator.getMovement(), is(Elevator.MOVEMENT.STILL));
+    }
+
+    @Test
+    public void engineWentDownEventChangesCurrentFloor(){
+        synchronizedElevator.setWantedFloor(START_FLOOR - 1);
+        synchronizedEngine.goDownOneFloor();
+        assertThat(synchronizedElevator.getCurrentFloor(), is(START_FLOOR - 1));
+    }
+
+    @Test
+    public void engineWentUpEventChangesCurrentFloor(){
+        synchronizedElevator.setWantedFloor(START_FLOOR - 1);
+        synchronizedEngine.goUpOneFloor();
+        assertThat(synchronizedElevator.getCurrentFloor(), is(START_FLOOR + 1));
+    }
+
+    @Test
+    public void wantedFloorDoesNotChangeWhenIncompatibleRequestReceived() {
+        elevator.requestElevatorToFloor(START_FLOOR - 3);
+        elevator.requestElevatorToFloor(START_FLOOR + 2);
+        assertThat(elevator.getWantedFloor(), is(START_FLOOR - 3));
     }
 }
